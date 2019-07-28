6b9f0745dd91bed798f98972599b967a344833f3
royekorolik <royekorolik7@gmail.com>
2019-07-28 20:52:03 +0300

    added gui

---
+++ gui.py
@@ -0,0 +1,24 @@
+import kivy
+#import App
+from kivy.app import App
+# Import Widgets
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+
+class DailyScheduale(Widget):
+    email = ObjectProperty(None)
+
+
+class MyApp(App):
+    def build(self):
+        return DailyScheduale()
+
+#Run App
+if __name__ == "__main__":
+    MyApp().run()
